ef7b2bdd2bf5435283e70c8f7ab0f277bc30dedf
param-ninthu <ninthu1999@gmail.com>
2023-06-22 03:01:20 +0530

    motion is working

--- src/components/Aboutus.jsx
+++ src/components/Aboutus.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { motion } from "framer-motion";
+import { slideIn, staggerContainer } from "../utils/motion";
+import "./../assets/style/css/about_us.css";
 import {
   AboutContainer,
   SectionHeading,
@@ -12,18 +15,30 @@ import about from "./../assets/images/about.jpg";
 const AboutUs = () => {
   return (
     <>
-      <Container $mode="about">
+      <motion.Container
+        className="about"
+        variants={staggerContainer()}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: true, amount: 0.25 }}
+      >
         <ImageBackground />
-        <AboutContainer>
-          <SectionHeading>About Us</SectionHeading>
-          <AboutParagraphContainer>
-            <SectionParagraph>
-              Lorem ipsum dolor sit amet, enna kodumai sir ithuhuiuh consectetur
-              adipiscing elit. Cras congue, erat a aliquam dapibus.
-            </SectionParagraph>
-          </AboutParagraphContainer>
-        </AboutContainer>
-      </Container>
+        <motion.div
+          variants={slideIn("right", "tween", 0.2, 1)}
+          className="about-container"
+        >
+          <AboutContainer>
+            <SectionHeading>About Us</SectionHeading>
+            <AboutParagraphContainer>
+              <SectionParagraph>
+                Lorem ipsum dolor sit amet, enna kodumai sir ithuhuiuh
+                consectetur adipiscing elit. Cras congue, erat a aliquam
+                dapibus.
+              </SectionParagraph>
+            </AboutParagraphContainer>
+          </AboutContainer>
+        </motion.div>
+      </motion.Container>
     </>
   );
 };

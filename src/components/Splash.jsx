565983a7ad4e63957318e56059f74e569f7680b4
param-ninthu <ninthu1999@gmail.com>
2023-06-23 19:31:01 +0530

    optional

--- src/components/Splash.jsx
+++ src/components/Splash.jsx
@@ -16,6 +16,13 @@ import splash from "./../assets/images/splash.jpg";
 import vidBg from "./../assets/video/bgVid.mp4";
 import "./../assets/style/css/cube.scss";
 
+import i1 from "./../assets/images/cubeicons/1.png";
+import i2 from "./../assets/images/cubeicons/2.png";
+import i3 from "./../assets/images/cubeicons/3.png";
+import i4 from "./../assets/images/cubeicons/4.png";
+import i5 from "./../assets/images/cubeicons/5.png";
+import i6 from "./../assets/images/cubeicons/6.png";
+
 const Splash = () => {
   let { scrollYProgress } = useScroll();
   let yRange = useTransform(scrollYProgress, [0, 1], ["0%", "50%"]);
@@ -38,22 +45,34 @@ const Splash = () => {
           <div className="stage-cube-cont">
             <div className="cubespinner">
               <div className="face1">
-                <div>Hi</div>
+                <div>
+                  <img src={i1} height="100px" width="100px" />
+                </div>
               </div>
               <div className="face2">
-                <div>Hi</div>
+                <div>
+                  <img src={i2} height="100px" width="100px" />
+                </div>
               </div>
               <div className="face3">
-                <div>Hi</div>
+                <div>
+                  <img src={i3} height="100px" width="100px" />
+                </div>
               </div>
               <div className="face4">
-                <div>Hi</div>
+                <div>
+                  <img src={i4} height="100px" width="100px" />
+                </div>
               </div>
               <div className="face5">
-                <div>Hi</div>
+                <div>
+                  <img src={i5} height="100px" width="100px" />
+                </div>
               </div>
               <div className="face6">
-                <div>Hi</div>
+                <div>
+                  <img src={i6} height="100px" width="100px" />
+                </div>
               </div>
             </div>
           </div>

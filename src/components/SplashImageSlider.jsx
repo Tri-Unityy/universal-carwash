7e12202dc72a212f80ab3927c2f8619a7934eb07
param-ninthu <ninthu1999@gmail.com>
2023-06-23 23:56:47 +0530

    working on alignments

--- src/components/SplashImageSlider.jsx
+++ src/components/SplashImageSlider.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 import "./../assets/style/css/splash-image-slider.css";
+import about from "./../assets/images/about.jpg";
 const SplashImageSlider = () => {
   return (
     <div className="container">
-      <div className="slider"></div>
+      <div className="slider">
+        <div className="slide-image">
+          <img src={about} />
+        </div>
+        <div className="slider-content">
+          <h2> Exterior Cleaning </h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+          </p>
+        </div>
+      </div>
     </div>
   );
 };

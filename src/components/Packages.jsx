62c79e6f67197bf5d9af0ee007a219a1c1ac1439
param-ninthu <ninthu1999@gmail.com>
2023-07-02 23:39:25 +0530

    working on slider

--- src/components/Packages.jsx
+++ src/components/Packages.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Container } from "../assets/style/homeElements";
 import Slider from "./Slider";
-
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 const Packages = () => {
   return (
     <Container $mode="packages">
+      <ArrowBackIosIcon />
       <Slider />
+      <ArrowForwardIosIcon />
     </Container>
   );
 };

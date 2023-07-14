13632ff20f06e33835dd3f52e63424346983c4bb
param-ninthu <ninthu1999@gmail.com>
2023-07-15 01:45:56 +0530

    have to find alternative for slider

--- src/components/Aboutus.jsx
+++ src/components/Aboutus.jsx
@@ -11,15 +11,6 @@ import {
   AboutParagraphContainer,
 } from "../assets/style/homeElements";
 
-import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-
-import { Pagination, Navigation } from "swiper/swiper.esm";
-
 import about from "./../assets/images/about.jpg";
 import exterior from "./../assets/images/exterior.jpg";
 import {
@@ -39,40 +30,29 @@ const AboutUs = () => {
           viewport={{ once: true, amount: 0.25 }}
         >
           <motion.div variants={slideIn("left", "tween", 0.0, 1)}>
-            <Swiper
-              pagination={{
-                type: "fraction",
-              }}
-              navigation={true}
-              modules={[Pagination, Navigation]}
-              className="mySwiper"
-            >
-              <SwiperSlide>
-                <ReactCompareSlider
-                  style={{ width: "100%", height: "70%" }}
-                  itemOne={
-                    <ReactCompareSliderImage
-                      src={about}
-                      style={{
-                        width: "100%",
-                        height: "100%",
-                        objectFit: "fill",
-                      }}
-                    />
-                  }
-                  itemTwo={
-                    <ReactCompareSliderImage
-                      src={exterior}
-                      style={{
-                        width: "100%",
-                        height: "100%",
-                        objectFit: "fill",
-                      }}
-                    />
-                  }
+            <ReactCompareSlider
+              style={{ width: "100%", height: "70%" }}
+              itemOne={
+                <ReactCompareSliderImage
+                  src={about}
+                  style={{
+                    width: "100%",
+                    height: "100%",
+                    objectFit: "fill",
+                  }}
                 />
-              </SwiperSlide>
-            </Swiper>
+              }
+              itemTwo={
+                <ReactCompareSliderImage
+                  src={exterior}
+                  style={{
+                    width: "100%",
+                    height: "100%",
+                    objectFit: "fill",
+                  }}
+                />
+              }
+            />
           </motion.div>
           <motion.div variants={textVariant()} className="about-container">
             <AboutContainer>

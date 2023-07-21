0d931f1a3e1d93c952fd028c56fba6a58eaccec2
mtbstn <thushibstn24@gmail.com>
2023-07-21 22:06:59 +0530

    add card content

---
+++ src/constants/packageEnglish.jsx
@@ -0,0 +1,71 @@
+const packagesE = [
+    {
+      packageId: "1",
+  
+      packageType: "Bike Normal",
+  
+      packagePrice: "25 CHF onwards",
+  
+      packageDuration: "30 mins",
+  
+      packageItems: {
+        item1: "100% Hand-cleaning",
+      },
+    },
+    {
+      packageId: "2",
+  
+      packageType: "Bike Premium",
+  
+      packagePrice: "100 CHF onwards",
+  
+      packageDuration: "45 mins",
+  
+      packageItems: {
+        item1: "100% Hand-cleaning",
+        item2: "Scratch repair"
+      },
+    },
+    {
+      packageId: "3",
+  
+      packageType: "4 x 4 Normal",
+  
+      packagePrice: "60 CHF onwards",
+  
+      packageDuration: "1 hr",
+  
+      packageItems: {
+        item1: "Interior",
+        item2: "Exterior",
+      },
+    },
+    {
+      packageId: "4",
+  
+      packageType: "4 x 4 Premium",
+  
+      packagePrice: "150 CHF onwards",
+  
+      packageDuration: "1 hr",
+  
+      packageItems: {
+        item1: "Interior",
+        item2: "Exterior",
+      },
+    },
+    {
+      packageId: "5",
+  
+      packageType: "Rim Cleaning",
+  
+      packagePrice: "100 CHF onwards",
+  
+      packageDuration: "30 mins",
+  
+      packageItems: {
+        item1: "Complete removal & cleaning",
+      },
+    },
+  ];
+  export default packagesE;

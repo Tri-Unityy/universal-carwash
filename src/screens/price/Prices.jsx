b07e4f16ebc8de27d3bf9afdcd2c286a9de899d6
chosen <67898034+param-ninthu@users.noreply.github.com>
2023-08-17 17:20:27 +0530

    finished cards

--- src/screens/price/Prices.jsx
+++ src/screens/price/Prices.jsx
@@ -1,11 +1,36 @@
 import React from "react";
-import { Container, PriceContainer } from "../../assets/style/priceElements";
+import {
+  CardContainer,
+  Cards,
+  Container,
+  PriceContainer,
+  PriceHeading,
+  PriceHeadingContainer,
+  TabContainer,
+  Tabs,
+} from "../../assets/style/priceElements";
 
 const Prices = () => {
   return (
     <>
       <Container $mode="price">
-        <PriceContainer>Price</PriceContainer>
+        <PriceContainer>
+          <PriceHeadingContainer>
+            <PriceHeading data-text="Packages">Packages</PriceHeading>
+          </PriceHeadingContainer>
+          <TabContainer>
+            <Tabs> Tab 1 </Tabs>
+            <Tabs> Tab 1 </Tabs>
+            <Tabs> Tab 1 </Tabs>
+            <Tabs> Tab 1 </Tabs>
+          </TabContainer>
+          <CardContainer>
+            <Cards>Cards</Cards>
+            <Cards>Cards</Cards>
+            <Cards>Cards</Cards>
+            <Cards>Cards</Cards>
+          </CardContainer>
+        </PriceContainer>
       </Container>
     </>
   );

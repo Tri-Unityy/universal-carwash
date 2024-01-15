134c100859c312c53a344d1d191a020b4a7f94b5
ninthu-hologo <ninthu1999@gmail.com>
2024-01-15 17:03:31 +0530

    have to work on form

--- src/assets/style/bookingElements.jsx
+++ src/assets/style/bookingElements.jsx
@@ -40,5 +40,60 @@ export const LeftContainer = styled.div`
     flex-direction: column;
     width: 80%;
     height: 90%;
-    border: 1px solid red;
+    /* border: 1px solid red; */
+`
+
+
+export const FormContainer = styled.form`
+    display: grid;
+    grid-template-columns: 50% 50%;
+    width: 100%;
+    height: 70%;
+    justify-content: center;
+`
+
+export const FormFields = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100px;
+  /* border: 1px solid red; */
+`
+
+export const Label = styled.label`
+  font-size: 15px;
+  font-weight: 500;
+`
+
+export const InputField = styled.input`
+  width: 90%;
+  height: 40px;
+  margin-top: 5px;
+  color: #0d0d0d;
+  background: transparent;
+  border-color: #e5e5e5;
+  padding-left: 5px;
+  border-radius: 5px;
+`
+
+export const SelectField = styled.select`
+  width: 90%;
+  height: 40px;
+  margin-top: 5px;
+  color: #0d0d0d;
+  background: transparent;
+  border-color: #e5e5e5;
+  padding-left: 5px;
+  border-radius: 5px;
+`
+
+export const Option = styled.option`
+  width: 90%;
+  height: 40px;
+  margin-top: 40px;
+  color: #0d0d0d;
+  background: transparent;
+  border-color: #e5e5e5;
+  padding-left: 5px;
+  border-radius: 5px;
 `
--- src/screens/booking/BookingPage.jsx
+++ src/screens/booking/BookingPage.jsx
@@ -1,17 +1,94 @@
 import React from "react";
-import { Container, LeftContainer } from "../../assets/style/bookingElements";
+import {
+  Container,
+  FormContainer,
+  FormFields,
+  InputField,
+  Label,
+  LeftContainer,
+  Option,
+  SelectField,
+} from "../../assets/style/bookingElements";
+
+import "./../../assets/style/css/booking-form.css"
+
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 const BookingPage = () => {
   return (
     <>
-   <Container $mode ="main" >
-    <Container $mode="left">
-      <LeftContainer>
-        Left
-      </LeftContainer>
-    </Container>
-    <Container $mode="right">right</Container>
-   </Container>
+      <Container $mode="main">
+        <Container $mode="left">
+          <LeftContainer>
+            <FormContainer>
+            <Form.Group className=" form-group" controlId="formName">
+                  <Form.Label
+                    className=" form-label"
+                    style={{ marginTop: "20px" }}
+                  >
+                    Name
+                  </Form.Label>
+                  <Form.Control
+                    required
+                    name="name"
+                    className="form-control-booking"
+                  
+                    type="text"
+                    placeholder="Enter Name"
+                  />
+                </Form.Group>
+
+                <Form.Group className=" form-group" controlId="formNumber">
+                  <Form.Label className=" form-label">Phone Number</Form.Label>
+                  <Form.Control
+                    required
+                    type="text"
+                    className="form-control-booking"
+              
+                    name="number"
+                 
+                    placeholder="Enter Phone Number"
+                  />
+                </Form.Group>
+
+                <Form.Group className=" form-group" controlId="setDate">
+                  <Form.Label className=" form-label">Select Date</Form.Label>
+                  <Form.Control
+                    required
+                    type="date"
+                    name="message"
+                    className="form-control-booking"
+                    
+                    placeholder="Schedule date"
+                  />
+                </Form.Group>
+                <Form.Group className=" form-group" controlId="setTime">
+                  <Form.Label className=" form-label">Select Time</Form.Label>
+                  <Form.Select
+                    required
+                    name="time"
+                   
+                    className="form-control-booking"
+                  
+                  >
+                    <option>Select your time slot</option>
+                    <option value="09 am - 10 am">09 am - 10 am</option>
+                    <option value="10 am - 11 am">10 am - 11 am</option>
+                    <option value="11 am - 12 pm">11 am - 12 pm</option>
+                    <option value="12 pm - 13 pm">12 pm - 13 pm</option>
+                    <option value="13 pm - 14 pm">13 pm - 14 pm</option>
+                    <option value="14 pm - 15 pm">14 pm - 15 pm</option>
+                    <option value="15 pm - 16 pm">15 pm - 16 pm</option>
+                    <option value="16 pm - 17 pm">16 pm - 17 pm</option>
+                    <option value="17 pm - 18 pm">17 pm - 18 pm</option>
+                  </Form.Select>
+                </Form.Group>
+            </FormContainer>
+          </LeftContainer>
+        </Container>
+        <Container $mode="right">right</Container>
+      </Container>
     </>
   );
 };
